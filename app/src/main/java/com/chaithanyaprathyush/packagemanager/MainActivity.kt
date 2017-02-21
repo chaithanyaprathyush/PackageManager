f46fc085ff7635ae7e21cd330fb828234d8b8a02
Chaithanya Prathyush <prathyush@witworks.com>
2017-02-21 19:13:01 +0530

    Add Dataset observer

--- app/src/main/java/com/chaithanyaprathyush/packagemanager/MainActivity.kt
+++ app/src/main/java/com/chaithanyaprathyush/packagemanager/MainActivity.kt
@@ -10,77 +10,84 @@ import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import java.util.*
 
-data class Application(val application: ApplicationInfo, val name: String, var uid: Int)
+data class Application(val application: ApplicationInfo, val name: String, val isSystemApp: Boolean, var uid: Int)
 
 class MainActivity: AppCompatActivity(), ListAdapter {
-        var arrayList = ArrayList<Application>()
-        var applicationManager: PackageManager? = null
-        var applicationListView: ListView? = null
-        private val mDataSetObservable = DataSetObservable()
+
+        private lateinit var applicationManager: PackageManager
+        private lateinit var applicationListView: ListView
+        private var applicationList: List<Application> = emptyList()
+        private val dataSetObservable = DataSetObservable()
+
     // Activity Overrides.
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             applicationManager = this.packageManager
             applicationListView = ListView(this)
-            applicationListView?.adapter = this
+            applicationListView.adapter = this
             setContentView(applicationListView)
             Thread(Runnable { getAppsList() }).start()
         }
         // Adapter Overrides.
-        override fun isEmpty(): Boolean { return arrayList.isEmpty() }
+        override fun isEmpty() = applicationList.isEmpty()
 
-        override fun getCount(): Int { return arrayList.count() }
+        override fun getCount(): Int = applicationList.count()
 
-        override fun hasStableIds(): Boolean { return true }
+        override fun hasStableIds() = true
 
-        override fun getItem(p0: Int): Application { return arrayList[p0] }
+        override fun getItem(p0: Int) = applicationList[p0]
 
-        override fun getItemId(p0: Int): Long { return arrayList[p0].uid.toLong() }
+        override fun getItemId(p0: Int) =  applicationList[p0].uid.toLong()
 
-        override fun getViewTypeCount(): Int {  return 1 }
+        override fun getViewTypeCount() = 1
 
-        override fun getItemViewType(p0: Int): Int { return 0 }
+        override fun getItemViewType(p0: Int) = 0
 
         override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View? {
             // Obtain the item associated with the position of this row in the list
             // Create a view to show the data associated with the item.
-
+            val application = getItem(p0)
+            val isSystemApp = if (application.isSystemApp) "${application.name} is a system app"
+                                            else "${application.name} is not a system app"
             if (p1 is TextView) {
                 Log.i("Adapter", "Reused A View")
-                p1.text = getItem(p0).name
+                p1.text = isSystemApp
                 return p1
             } else {
                 Log.i("Adapter", "Created A View")
                 val textView = TextView(this)
                 textView.setPadding(25, 25, 25, 25)
-                textView.text = getItem(p0).name
+                textView.text = isSystemApp
                 return textView
             }
         }
 
-        override fun registerDataSetObserver(p0: DataSetObserver?) {
-            mDataSetObservable.registerObserver(p0)
-        }
+        override fun registerDataSetObserver(p0: DataSetObserver?) = dataSetObservable.
+                                                                            registerObserver(p0)
 
-        override fun unregisterDataSetObserver(p0: DataSetObserver?) {
-            mDataSetObservable.unregisterObserver(p0)
-        }
+        override fun unregisterDataSetObserver(p0: DataSetObserver?) = dataSetObservable.
+                                                                            unregisterObserver(p0)
 
-        override fun areAllItemsEnabled(): Boolean { return true }
+        override fun areAllItemsEnabled() = true
 
         // List Adapter Overrides
-        override fun isEnabled(p0: Int): Boolean { return true }
+        override fun isEnabled(p0: Int) = true
 
         private fun getAppsList() {
-            val applicationList =
-                    applicationManager?.getInstalledApplications(PackageManager.GET_META_DATA)
-            arrayList = applicationList?.map {
-                val applicationName = applicationManager?.getApplicationLabel(it).toString()
+            val applicationData =
+                    applicationManager.getInstalledApplications(PackageManager.GET_META_DATA)
+             applicationList = applicationData?.map {
+                val applicationName = applicationManager.getApplicationLabel(it).toString()
                 Log.i("Application Info", "Application Name: $applicationName, Package Name: ${it.packageName}")
-                Application(it, applicationName, it.uid)
-            } as? ArrayList<Application> ?: ArrayList<Application>()
-            runOnUiThread { mDataSetObservable.notifyChanged() }
+                if (it.flags and ApplicationInfo.FLAG_SYSTEM != 0) {
+                    Log.i("Application Info", "$applicationName is system app")
+                    Application(it, applicationName, true, it.uid)
+                } else {
+                    Log.i("Application Info", "$applicationName is not a system app")
+                    Application(it, applicationName, false, it.uid)
+                }
+            } ?: emptyList()
+            runOnUiThread { dataSetObservable.notifyChanged() }
         }
 }
